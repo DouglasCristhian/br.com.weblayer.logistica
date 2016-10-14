99c04c3c166d7afc8a8287114e8a0109243c258f
DouglasCristhian (Note) <dc@notebook>
2016-10-14 15:39:20 -0300

    --

---
+++ br.com.weblayer.logistica.core/BLL/UsuarioManager.cs
@@ -0,0 +1,18 @@
+﻿public class UsuarioManager
+{ 
+
+	public static Usuario ExecutarLogin(string servidor, string login, string senha)
+	{
+
+		//Acessar serviço remoto e validar usuário.
+
+		var usuario = new Usuario();
+		usuario.login = login;
+		usuario.senha = senha;
+		usuario.nome = "Nome retornado do banco de dados";
+
+		return usuario;
+
+	}
+
+}
---
+++ br.com.weblayer.logistica.core/Model/Usuario.cs
@@ -0,0 +1,22 @@
+﻿public class Usuario
+{ 
+ 	
+	public virtual string nome
+	{
+		get;
+		set;
+	}
+
+	public virtual string login
+	{
+		get;
+		set;
+	}
+
+	public virtual string senha
+	{
+		get;
+		set;
+	}
+
+}
