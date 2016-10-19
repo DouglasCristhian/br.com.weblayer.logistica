d647995a36b114530144c85f792bc1735b37aa29
DouglasCristhian (NoteBook DELL) <dc@weblayer.com.br>
2016-10-19 14:06:06 -0200

    classe de nota

--- br.com.weblayer.logistica.core/BLL/UsuarioManager.cs
+++ br.com.weblayer.logistica.core/BLL/UsuarioManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using br.com.weblayer.logistica.core.Model;
+using System.Collections.Generic;
 
 namespace br.com.weblayer.logistica.core.BLL
 {
--- br.com.weblayer.logistica.core/Model/Usuario.cs
+++ br.com.weblayer.logistica.core/Model/Usuario.cs
@@ -1,21 +1,24 @@
-﻿public class Usuario
-{ 
- 	
-	public virtual int id_empresa
-	{ get; set; }
+﻿namespace br.com.weblayer.logistica.core.Model
+{
+    public class Usuario
+    {
 
-	public virtual int id_usuario
-	{ get; set; }
+        public virtual int id_empresa
+        { get; set; }
 
-	public virtual string ds_empresa
-	{ get; set; }
+        public virtual int id_usuario
+        { get; set; }
 
-	public virtual string ds_perfil
-	{ get; set; }
+        public virtual string ds_empresa
+        { get; set; }
 
-	public virtual int id_transportadora
-	{ get; set; }
+        public virtual string ds_perfil
+        { get; set; }
 
-	public virtual string ds_transportadora
-	{ get; set; }
+        public virtual int id_transportadora
+        { get; set; }
+
+        public virtual string ds_transportadora
+        { get; set; }
+    }
 }
